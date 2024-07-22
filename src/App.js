4eb536ab7b9107c19e409d708a33f570c7c528e7
mohamed saad <ma6118923@gmail.com>
2024-07-22 11:52:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [cityName, setCityName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const getWeather = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6381c182fea334510f3d0e2aa8f720e4&units=metric`);
+      setData(response.data.main);
+    } catch (error) {
+      console.error("Error fetching the weather data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <input 
+          type="text" 
+          placeholder="Enter the city name" 
+          onChange={e => setCityName(e.target.value)}
+        />
+        <button onClick={getWeather}>
+          Get Weather
+        </button>
+
+        {loading && <h1>Loading...</h1>}
+        {data ? (
+          <div className="Weather">
+            <h2>Weather Data</h2>
+            <h3>Temperature: {data.temp} °C</h3>
+            <h3>Humidity: {data.humidity}%</h3>
+            <h3>Pressure: {data.pressure} hPa</h3>
+          </div>
+        ) : (
+          <h2>Enter a city name</h2>
+        )}
+      </div>
     </div>
   );
 }
